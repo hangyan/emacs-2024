20ccd5db174df0c3e5cb39e3b817abf7a78b64ca
Hang Yan <yhang@vmware.com>
2023-10-26 19:26:30 +0800

    init
    Signed-off-by: Hang Yan <yhang@vmware.com>

--- custom-file.el
+++ custom-file.el
@@ -3,11 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(misterioso))
- '(package-selected-packages '(company)))
+ '(custom-enabled-themes '(tsdh-dark))
+ '(package-selected-packages
+   '(imenu-list imenu-anywhere lsp-mode go-mode yaml-mode helm company))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "PT Mono" :foundry "nil" :slant normal :weight regular :height 140 :width normal)))))
--- init.el
+++ init.el
@@ -10,6 +10,15 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
+;; install these packages
+;; helm yaml-mode imenu-anywhere iemnu-list
+
+
+(setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
+
+
 (setq custom-file "~/.emacs.d/custom-file.el")
 
 (load-file custom-file)
@@ -22,3 +31,42 @@
 
 ;; Provide instant autocompletion.
 (setq company-idle-delay 0.0)
+
+
+;; helm
+(require 'helm)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-xref-candidate-formatting-function 'helm-xref-format-candidate-full-path)
+(recentf-mode 1)
+(setq-default recent-save-file "~/.emacs.d/recentf")  
+(helm-recentf)
+(define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
+(define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") #'helm-select-action)
+
+
+;; window size
+(add-to-list 'default-frame-alist '(height . 120))
+(add-to-list 'default-frame-alist '(width . 160))
+
+;; backup file
+(setq make-backup-files nil)
+
+
+;; imenu
+(defun try-to-add-imenu ()
+  (condition-case nil (imenu-add-to-menubar "yourFancyName") (error nil)))
+(add-hook 'font-lock-mode-hook 'try-to-add-imenu)
+
+ (defun my-imenu-rescan ()
+   (interactive)
+   (imenu--menubar-select imenu--rescan-item))
+(global-set-key "\C-cI" 'my-imenu-rescan)
+(global-set-key (kbd "C-.") 'imenu-anywhere)
+(global-set-key (kbd "C-'") #'imenu-list-smart-toggle)
+
+
+(global-display-line-numbers-mode)
